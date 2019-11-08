46b4b38aec1f6b344f724ed70af06306f2e6ae35
Andrii Ivanytskyi <andriiivanytskyi@gmail.com>
2019-11-08 11:20:44 -0800

    added book_authors project using SQL and Materialize

--- Books_Authors/apps/books_authors_app/urls.py
+++ Books_Authors/apps/books_authors_app/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.add_book),
+    url(r'^add_book_process$', views.add_book_process),
+    url(r'^books/(?P<id>\d+)$', views.add_book_description),
+    url(r'^books/book_description_process/(?P<book_id>\d+)$', views.book_description_process),
 ]
--- Books_Authors/apps/books_authors_app/views.py
+++ Books_Authors/apps/books_authors_app/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Book, Author
 
-def index(request):
-  return HttpResponse('pidr')
+def add_book(request):
+  context = {
+    'books': Book.objects.all()
+  }
+  return render(request, 'books_authors_app/add_book.html', context)
+
+def add_book_process(request):
+  title = request.GET['title']
+  description = request.GET['description']
+  Book.objects.create(title=title, description=description)
+  return redirect('/')
+
+def add_book_description(request, id):
+  context = {
+    'book': Book.objects.get(id=id),
+    'authors': Book.objects.get(id=id).authors.all(),
+    'all_authors': Author.objects.all(),
+  }
+  return render(request, 'books_authors_app/book_description.html', context)
+
+def book_description_process(request, book_id):
+  selected_author_id = request.GET['add_author']
+  Author.objects.get(id=selected_author_id).books.add(Book.objects.get(id=book_id))
+  return redirect('/books/' + book_id)
